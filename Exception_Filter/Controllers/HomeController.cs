4d60bf2b3954491cef0df4f1c798ce49e358f3d6
Admin <Admin@DESKTOP-5789TGK>
2024-03-23 10:45:37 +0300

    Add project files.

---
+++ Exception_Filter/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Exception_Filter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Exception_Filter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            int a = 0;
+            int b = 10;
+            int bolum = b / a;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Exception_Filter/CustomExceptionFilter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using System.Net.Mail;
+
+namespace Exception_Filter
+{
+    // IException Filter interfacesinden kalıtmammız gerekmektedir.
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            // Exception Filter : Uygulamanın herhangi bir yerinde exception olduğunda, buraya düşecek demektir
+            // Uygulamalarınızın exceptionlarını takip edebilmek için, Log kaydı atabilir, Bildirim için email gönderebilirsiniz.
+
+            // Exception olduğunda size email atan bir sistem yapalım 
+
+
+            // Yakalanan exceptionları, nasıl yakalayabilirim.
+            //context.Exception;
+
+            var ex = context.Exception;
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.Credentials = new NetworkCredential("wissenfsd@gmail.com", "Bright**");
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = true;
+
+            MailMessage message = new MailMessage("wissenfsd@gmail.com", "serhatebren34@gmail.com");
+
+            message.Subject = "Web Sitenden hata alındı";
+            message.Body = "Web Siteden, şu hata alındı : " + ex.Message;
+            
+            //message.To.Add(new MailAddress("serhatebren34@gmail.com"));
+
+            //email message hazır gönderelim
+
+            client.Send(message);
+
+
+        }
+    }
+}
